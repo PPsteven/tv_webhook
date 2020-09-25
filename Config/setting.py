0d29deb77adc3dc342f00c7ab31033888f245760
PPsteven <35828860+PPsteven@users.noreply.github.com>
2020-09-25 12:27:16 +0800

    Update setting.py

--- Config/setting.py
+++ Config/setting.py
@@ -15,11 +15,11 @@ SERVER_API = '0.0.0.0'
 SERVER_PORT = 5566
 
 TOKEN = [
-        'test123',
+        'test123434',
          ]
 
 WECHAT_CONFIG = {
-    'CORPID': 'ww357e38c68455ebd9',
+    'CORPID': 'ww357e38c6111455ebe9',
     'CORPSECRET': 'oeVkBNFkeAJADZQEw0Pl2SWz8R8CZRmO8Hd3AXoCdB0',
     'ACCESS_TOKEN_API': 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={ID}&corpsecret={SECRET}',
     'MESSAGE_SEND_API': 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={ACCESS_TOKEN}',
@@ -38,17 +38,17 @@ WECHAT_CONFIG = {
 }
 
 EMAIL_CONFIG = {
-    'ACCOUNT': 'goldmarket123@126.com',
-    'PASSWORD': 'goldmarket123',
-    'SENDER': 'goldmarket123@126.com',
-    'RECEIVERS': ['goldmarket123@126.com', '932163071@qq.com']
+    'ACCOUNT': 'your_mail@xx.com',
+    'PASSWORD': 'xxx',
+    'SENDER': 'your_mail@xx.com',
+    'RECEIVERS': ['your_mail@xx.com', 'your_mail2@xx.com']
 }
 
 # 使用短信宝 http://www.smsbao.com/login
 # PATTERN 模板需要自己申请
 SMSBOX_CONFIG = {
-    'ACCOUNT': 'ppsteven',
-    'PASSWORD': '123123',
-    'DEFAULT_RECEIVERS': [13816715280],
+    'ACCOUNT': 'xxxx',
+    'PASSWORD': 'xxxx',
+    'DEFAULT_RECEIVERS': [138111111111],
     'PATTERN': '【魔术师】当前时间: {time} ;{name} 提醒您: {message}'
 }
